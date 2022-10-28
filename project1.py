7742634c4da720b650a51077f9ee9faea4bac878
Ravika2410 <75975415+Ravika2410@users.noreply.github.com>
2022-10-28 20:48:01 +0530

    Update project1.py

--- project1.py
+++ project1.py
@@ -3,3 +3,12 @@ if a % 4==0 or a % 400==0 and a % 100!=0:
     print("This is leap year.")
 else:
     print("This is not leap year.")
+    
+    b=int(input("Enter the height in centimeter."))
+inches=0.394 
+print(("The length in inches :",round(b*inches,2),"inches"))
+
+
+c=int(input("Enter the height in centimeter."))
+feet=0.0328 
+print(("The length in feet :",round(c*feet,2),"feet"))
